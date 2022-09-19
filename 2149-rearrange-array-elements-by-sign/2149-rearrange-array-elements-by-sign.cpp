c1ddd56923e9620f218ffe5142e26071f289b4ad
Anuj Singh <80690679+Anuj-Rathore24@users.noreply.github.com>
2022-09-19 15:43:30 +0530

    Time: 303 ms (71.13%), Space: 123.9 MB (86.06%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int> even,odd;
-        for(int i=0;i<nums.size();i++){
-            int elements=nums.at(i);
-            if(elements>0) even.push(elements);
-            else odd.push(elements);
-        }
-        int i=0;
-        while(i<nums.size()){
-            if(i%2==0) {
-                nums.at(i)=even.front();
-                even.pop();    
-            }else{
-                nums.at(i)=odd.front();
-                odd.pop();
+vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        int indexpos = 0, indexneg=1;
+        for(auto num: nums){
+            if(num>0){
+                ans[indexpos] = num;
+                indexpos+=2;
             }
-            i++;
+            if(num<0){
+                ans[indexneg] = num;
+                indexneg += 2;
+            }
         }
-        return nums;
+        return ans;
+        
     }
 };
