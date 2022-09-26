3bf49d43f46f3d76b0e68baad999dffe25057172
Anuj Singh <80690679+Anuj-Rathore24@users.noreply.github.com>
2022-09-26 17:17:32 +0530

    Time: 51 ms (25.09%), Space: 16.9 MB (86.28%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> table;
-        int result;
-        for(int i:nums){
-            auto itr=table.find(i);
-            if(itr==table.end()){
-                table.insert({i,1});
-            }else{
-                itr->second++;
-            }
-        }
-        for(auto itr:table){
-            if(itr.second==1) return itr.first;
-        }
-        return result;
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
     }
 };
