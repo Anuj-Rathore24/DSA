97c48e8bec00d6d1757bd7c152ad723f6d18d305
Anuj Singh <80690679+Anuj-Rathore24@users.noreply.github.com>
2023-02-17 16:10:35 +0530

    Minimum Distance Between BST

---
+++ MinimumDistanceBetweenBST.cpp
@@ -0,0 +1,49 @@
+
+/*
+Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+
+ 
+
+Example 1:
+
+Input: root = [4,2,6,1,3]
+Output: 1
+
+Example 2:
+
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+ 
+
+*/
+
+
+class Solution {
+public:
+    // List to store the tree nodes in the inorder traversal.
+    vector<int> inorderNodes;
+    
+    void inorderTraversal(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        
+        inorderTraversal(root->left);
+        // Store the nodes in the list.
+        inorderNodes.push_back(root->val);
+        inorderTraversal(root->right);
+    }
+    
+    int minDiffInBST(TreeNode* root) {
+        inorderTraversal(root);
+        
+        int minDistance = INT_MAX;
+        // Find the diff between every two consecutive values in the list.
+        for (int i = 1; i < inorderNodes.size(); i++) {
+            minDistance = min(minDistance, inorderNodes[i] - inorderNodes[i - 1]);
+        }
+        
+        return minDistance;
+    }
+};
